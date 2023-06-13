619abe4034a859bee0122ccdbb3d49721f91d761
rismarmdni23 <rismanurainiramadlani26p2@gmail.com>
2023-06-13 11:37:03 +0800

    Menjalankan Program

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -2,29 +2,41 @@
 package universitas;
 
 public class Mahasiswa {
-    String nama;
-    String npm;
-    String prodi;
-    
-    public String getNama(){
-       return nama;  
-   }
-    public String getNpm(){
-       return npm;  
-   }
-    public String getProdi(){
-       return prodi;  
-   }
-    
-    public void setnama (String nama){
-       nama = nama;
-   }
-    
-    public void setnpm (String npm){
-       npm = npm;
+    private String nama;
+    private String npm;
+    private String prodi;
+
+    public Mahasiswa(String npm, String nama, String prodi) {
+        this.nama = nama;
+        this.npm = npm;
+        this.prodi = prodi;
+    }
+  
+    public String setDetail(){
+        return npm + nama + prodi;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
     }
     
-    public void setrodi (String prodi){
-       prodi = prodi;
+    public String getNama() {
+        return nama;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNpm() {
+        return npm;
+    }
+    
+    public void setNpm(String npm) {
+        this.npm = npm;
     }
 }
--- src/universitas/MahasiswaBeraksi.java
+++ src/universitas/MahasiswaBeraksi.java
@@ -4,10 +4,15 @@ package universitas;
 public class MahasiswaBeraksi {
     public static void main(String[] args){
             
-    Mahasiswa mhs = new Mahasiswa;
+    Mahasiswa mhs = new Mahasiswa("2110010499", "Risma Nur Aini Ramadlani", " Teknik Informatika");
     
-    System.out.println("NPM:  "+mhs.setnpm());
-    System.out.println("Nama:  "+mhs.setnama());
-    System.out.println("Prodi:  "+mhs.setprodi());
+    
+        System.out.println("Nama: "+mhs.getNama());
+        System.out.println("NPM: "+mhs.getNpm());
+        System.out.println("Prodi: "+mhs.getProdi());
+        
+        System.out.println(mhs.setDetail());
+
+    
 }
 }
